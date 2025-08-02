e18650bb5e564cb18a210e3502577086df7964c7
Annafee-Pathan <pathanannafee@gmail.com>
2025-08-02 12:39:58 +0600

    Add files via upload

---
+++ Lines_in_File.py
@@ -0,0 +1,13 @@
+file = open('a.txt','r')
+Counter = 0
+
+Content = file.read()
+
+CoList = Content.split("\n")
+
+for i in CoList:
+    if i:
+        Counter += 1
+
+print("Lines:")
+print(Counter)
