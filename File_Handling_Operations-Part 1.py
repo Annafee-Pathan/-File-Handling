792bd3cb857efccf908a2b21288b339945b86abc
Annafee-Pathan <pathanannafee@gmail.com>
2025-08-02 12:32:53 +0600

    Add files via upload

--- File_Handling_Operations-Part 1.py
+++ File_Handling_Operations-Part 1.py
@@ -1,3 +1,12 @@
 file = open('a.txt','r')
 print(file.read())
 file.close()
+
+file_w=open('a.txt','w')
+file_w.write("Hello")
+file_w.close()
+
+file_append = open('a.txt','a')
+file_append.write("\n File in append mode .....")
+file_append.write("My name is penguin")
+file_append.close()
